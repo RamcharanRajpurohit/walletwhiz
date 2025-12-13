2d4492fe0b861aad6415dbb019b99be3226b3e88
ramcharanrajpurohit <b23ci1032@iitj.ac.in>
2025-12-13 16:44:03 +0530

    update nextjs version

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,6 +2,5 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 export const createServerClient = async () => {
-  const cookieStore = await cookies()
-  return createServerComponentClient({ cookies: () => cookieStore })
+  return createServerComponentClient({ cookies })
 }
